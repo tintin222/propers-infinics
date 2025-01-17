0f65c9c1f35c284fd46fd2eedff9c1303d77891b
tintin222 <lefeakman@gmail.com>
2025-01-17 12:47:01 +0300

    corrected twice message

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,5 @@ import { generateId } from 'ai'
 
 export default function HomePage() {
   const id = generateId()
-  return (
-    <div className="flex flex-col w-full max-w-3xl pt-10 pb-20 mx-auto stretch">
-      <Chat id={id} />
-    </div>
-  )
+  return <Chat id={id} />
 }
